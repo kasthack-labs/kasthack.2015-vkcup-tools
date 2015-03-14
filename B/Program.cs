870175038be0f52f410f66ef10e09d7ab7eec70c
kasthack <kasthack@epicm.org>
2015-03-14 22:01:42 +0300

    B.winrar

--- B/Program.cs
+++ B/Program.cs
@@ -1,52 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace B {
     class Program {
         static void Main( string[] args ) {
-            var qs = Console.In.ReadLine().Split( ' ' ).Select( double.Parse ).ToArray();
+            int T;
+            double c;
+            int[] requests;
+            int[] checks;
 
-            //var n = (int) qs[ 0 ];
-            var T = (int) qs[ 1 ];
-            var c = qs[ 2 ];
-            //var requests = new int[n];
-            //for ( int i = 0; i < n; i++ ) requests[ i ] = int.Parse( Console.In.ReadLine() );
-            var requests = Console.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
-            //var m = int.Parse( Console.In.ReadLine() );
-            Console.ReadLine();
-            //var checks = new int[m];
-            //for ( int i = 0; i < m; i++ ) checks[ i ] = int.Parse( Console.In.ReadLine() );
-            var checks = Console.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
-
+            var ir = Console.In;
+            var or = Console.Out;
+            var cult = CultureInfo.InvariantCulture;
+            {
+                var qs = ir.ReadLine().Split( ' ' );//.Select( double.Parse ).ToArray();
+                T = int.Parse( qs[1] );
+                c = double.Parse(qs[ 2 ],cult);
+            }
+            {
+                var r = ir.ReadLine().Split( ' ' );
+                requests = new int[r.Length];
+                for ( int i = 0; i < r.Length; i++ ) {
+                    requests[ i ] = int.Parse( r[ i ] );
+                }
+                r = null;
+            }
+            ir.ReadLine();
+            {
+                var r = ir.ReadLine().Split( ' ' );
+                checks = new int[r.Length];
+                for ( int i = 0; i < r.Length; i++ ) {
+                    checks[ i ] = int.Parse( r[ i ] );
+                }
+                r = null;
+            }
             int start = 0;
             decimal approx = 0;
-
             foreach ( var check in checks ) {
                 var real = Real( requests, check, T );
-
                 Approx( requests, start, check, T, c, ref approx );
-                start = check + 1;
-
+                start = check;
                 var error = Math.Abs( approx - (decimal) real ) / (decimal) real;
-
-                Console.Out.WriteLine( "{0:F5} {1:F5} {2:F5}", real, approx, error );
+                or.WriteLine( String.Format( cult, "{0:F6} {1:F6} {2:F6}", real, approx, error ));
             }
-            Console.ReadLine();
         }
 
         private static double Real(int[] requests, int time, int period) {
             long rc = 0;
-            for ( int i = time-period; i < time; i++ ) {
-                rc += requests[ i ];
-            }
-            return (double) rc / time;
+            for ( int i = Math.Max( 0, time - period ); i < time; i++ ) rc += requests[ i ];
+            return (double) rc / period;
         }
 
         private static void Approx( int[] request, int start, int end, double t, double c, ref decimal current ) {
-            for ( int i = start; i < end; i++ ) current = ( current + request[ i ] / (decimal) t ) / (decimal) c;
+            for ( int i = start; i < end; i++ )
+                current = ( current + request[ i ] / (decimal) t ) / (decimal) c;
         }
     }
 }

a52862d2b3bd412f6d2831fedb19484fcb7203e4
kasthack <kasthack@epicm.org>
2015-03-14 21:22:16 +0300

    d.temp, b.preclean

--- B/Program.cs
+++ B/Program.cs
@@ -7,6 +7,46 @@ using System.Threading.Tasks;
 namespace B {
     class Program {
         static void Main( string[] args ) {
+            var qs = Console.In.ReadLine().Split( ' ' ).Select( double.Parse ).ToArray();
+
+            //var n = (int) qs[ 0 ];
+            var T = (int) qs[ 1 ];
+            var c = qs[ 2 ];
+            //var requests = new int[n];
+            //for ( int i = 0; i < n; i++ ) requests[ i ] = int.Parse( Console.In.ReadLine() );
+            var requests = Console.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+            //var m = int.Parse( Console.In.ReadLine() );
+            Console.ReadLine();
+            //var checks = new int[m];
+            //for ( int i = 0; i < m; i++ ) checks[ i ] = int.Parse( Console.In.ReadLine() );
+            var checks = Console.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+
+            int start = 0;
+            decimal approx = 0;
+
+            foreach ( var check in checks ) {
+                var real = Real( requests, check, T );
+
+                Approx( requests, start, check, T, c, ref approx );
+                start = check + 1;
+
+                var error = Math.Abs( approx - (decimal) real ) / (decimal) real;
+
+                Console.Out.WriteLine( "{0:F5} {1:F5} {2:F5}", real, approx, error );
+            }
+            Console.ReadLine();
+        }
+
+        private static double Real(int[] requests, int time, int period) {
+            long rc = 0;
+            for ( int i = time-period; i < time; i++ ) {
+                rc += requests[ i ];
+            }
+            return (double) rc / time;
+        }
+
+        private static void Approx( int[] request, int start, int end, double t, double c, ref decimal current ) {
+            for ( int i = start; i < end; i++ ) current = ( current + request[ i ] / (decimal) t ) / (decimal) c;
         }
     }
 }
--- D/Program.cs
+++ D/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace D {
     class Program {
         static void Main( string[] args ) {
+            var sr = Console.In.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+            var vids = sr[ 0 ];
+            var srvs = sr[ 1 ];
+            var movs = ParseUploads( vids );
+            for ( int i = 0; i < movs.Count; i++ ) {
+                
+            }
         }
+        private static List<Upload> ParseUploads( int vids ) {
+            var movs = new List<Upload>( vids );
+            for ( int i = 0; i < vids; i++ ) {
+                var r = Console.In.ReadLine().Split( ' ' );
+                movs.Add(
+                    new Upload {
+                        Index = i,
+                        Date = int.Parse( r[ 0 ] ),
+                        Length = int.Parse( r[ 1 ] ),
+                        Complete = -1
+                    } );
+            }
+            return movs;
+        }
+    }
+
+    internal class ServerPool {
+        private readonly int _count;
+        private int _freeCount;
+        private Queue<Upload> _queue;
+        public ServerPool( int count ) {
+            _count = count;
+            _queue = new Queue<Upload>();
+        }
+        public void Add(Upload u) {
+            
+        }
+    }
+
+    class Upload {
+        public int Index { get; set; }
+        public int Date { get; set; }
+        public int Length { get; set; }
+        public int Complete { get; set; }
     }
 }

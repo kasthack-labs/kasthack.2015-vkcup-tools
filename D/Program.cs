b7a19fbd13239f913531e84030aa105411d7ad69
kasthack <kasthack@epicm.org>
2015-03-15 14:16:04 +0300

    D.win

--- D/Program.cs
+++ D/Program.cs
@@ -1,51 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
 namespace D {
     class Program {
-        static void Main( string[] args ) {
-            var sr = Console.In.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
-            var vids = sr[ 0 ];
-            var srvs = sr[ 1 ];
-            var movs = ParseUploads( vids );
-            for ( int i = 0; i < movs.Count; i++ ) {
-                
-            }
+        static void Main() {
+            var input = ParseStdIn();
+            Impl2(input);
+            Print( input );
         }
-        private static List<Upload> ParseUploads( int vids ) {
-            var movs = new List<Upload>( vids );
+
+        private static void Print( Input input ) {
+            var sb = new StringBuilder();
+            var uploads = input.uploads;
+            for ( int index = 0; index < uploads.Length; index++ )
+                sb.AppendLine( uploads[ index ].Complete.ToString() );
+            Console.Out.Write(sb.ToString());
+        }
+
+        private static Input ParseStdIn() {
+            var sr = Console.In.ReadLine().Split( ' ' );
+            var vids = int.Parse(sr[ 0 ]);
+            var srvs = int.Parse(sr[ 1 ]);
+            var movs = ParseUploads( vids );
+            var input = new Input { Servers = srvs, uploads = movs };
+            return input;
+        }
+
+        private static Upload[] ParseUploads( int vids ) {
+            var movs = new Upload[ vids ];
             for ( int i = 0; i < vids; i++ ) {
                 var r = Console.In.ReadLine().Split( ' ' );
-                movs.Add(
-                    new Upload {
+                movs[i] = new Upload {
                         Index = i,
                         Date = int.Parse( r[ 0 ] ),
                         Length = int.Parse( r[ 1 ] ),
                         Complete = -1
-                    } );
+                };
             }
             return movs;
         }
-    }
+        private static void Impl2( Input input ) {
+            long[] servers = new long[ input.Servers ];
 
-    internal class ServerPool {
-        private readonly int _count;
-        private int _freeCount;
-        private Queue<Upload> _queue;
-        public ServerPool( int count ) {
-            _count = count;
-            _queue = new Queue<Upload>();
+            long current = 0;
+            long start = 0;
+
+            var uploads = input.uploads;
+            var length = uploads.Length;
+
+            for ( int index = 0; index < length; index++ ) {
+                var upload = uploads[ index ];
+                l:
+                start = Math.Max( start, upload.Date );
+                if ( servers[ current ] <= start ) {
+                    upload.Complete = servers[ current ] = Math.Max( servers[ current ], upload.Date) + upload.Length;
+                    current = ( current + 1 ) % input.Servers;
+                    continue;
+                }
+                Array.Sort( servers ); //нужно что-то более эффективное, но пока сойдёт
+                start = servers[ 0 ];
+                current = 0;
+                goto l;
+            }
         }
-        public void Add(Upload u) {
-            
+
+        private static void swap( long[] servers, long i, long i1 ) {
+            var t = servers[ i ];
+            servers[ i ] = servers[ i1 ];
+            servers[ i1 ] = t;
         }
     }
-
+    class Input {
+        public int Servers;
+        public Upload[] uploads;
+    }
     class Upload {
-        public int Index { get; set; }
-        public int Date { get; set; }
-        public int Length { get; set; }
-        public int Complete { get; set; }
+        public int Index;
+        public long Date;
+        public long Length;
+        public long Complete;
     }
 }
